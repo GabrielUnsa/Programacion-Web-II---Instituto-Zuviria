4c2a6ba184fb5348126b0206193c6a9fe7b70f14
GabrielUnsa <gab24wal@gmail.com>
2025-03-17 18:56:53 -0300

    Agregamos los ejemplos de la primera clase

---
+++ Ejemplos Vistos en Clases/Ejemplos Clases 1/Emmet/example_doc.js
@@ -0,0 +1,10 @@
+/**
+ * For con errores para ejemplo de debug
+ * @date 2025-03-17
+ * @param { none } none
+ * @author Gabriel Marmanillo
+ */
+for (let i = 0; i < 5; i++) {
+    console.log(i);    
+    i=0;
+}
